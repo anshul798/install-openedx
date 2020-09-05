3c372ed6f980c0cd325eb0557125902076fdb48f
Anshul Bhardwaj <anshulbhardwaj798@gmail.com>
2020-09-06 01:22:00 +0530

    fixed openedx_install script

--- openedx_install.sh
+++ openedx_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Setup the System for installing openedx
-wget https://raw.githubusercontent.com/anshul798/install-openedx/master/env_setup.sh -O - | sudo -H bash
+wget https://raw.githubusercontent.com/anshul798/install-openedx/master/env_setup.sh -O - | bash
 
 # Change ansible configs
-wget https://raw.githubusercontent.com/anshul798/install-openedx/master/config_change.sh -O - | sudo -H bash
+wget https://raw.githubusercontent.com/anshul798/install-openedx/master/config_change.sh -O - | bash
 
 # install openedx
-wget https://raw.githubusercontent.com/anshul798/install-openedx/master/install.sh -O - | sudo -H bash > install.out
+wget https://raw.githubusercontent.com/anshul798/install-openedx/master/install.sh -O - | bash > install.out
 
